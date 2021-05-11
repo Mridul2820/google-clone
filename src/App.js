7f43badb3ef7eba5d0530128270e4c34887da54a
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-11 23:12:51 +0530

    routing and header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Home from './pages/Home'
 import './styles/app.scss'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
     return (
         <div className="app">
-            Google
-
-            <Home />
-
-            
+            <Router>
+                <Switch>
+                    <Route path="search">
+                        Google Search
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import AppsIcon from '@material-ui/icons/Apps';
+import Avatar from '@material-ui/core/Avatar';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    <Link to="about">About</Link>
+                    <Link to="store">Store</Link>
+                </div>
+                <div className="home__headerRight">
+                    <Link to="gmail">Gamil</Link>
+                    <Link to="images">Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+
+                </div>
+            </div>
+            <div className="home__body"></div>
         </div>
     )
 }
