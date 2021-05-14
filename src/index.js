8e45e481c25e8b3bc1966216c081745d61172337
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-14 12:37:59 +0530

    set up context api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reducer, {initialState} from './context/reducer'
+import { StateProvider } from './context/StateProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <StateProvider initialState={initialState} reducer={reducer}>
+            <App />
+        </StateProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
